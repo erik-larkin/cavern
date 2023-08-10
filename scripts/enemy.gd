1841a633733d1dc6f93afca8eed03c39c8aee4c4
erik-larkin <erik.larkin789@gmail.com>
2023-08-10 15:48:26 +0100

    Add animation tree for enemy

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -6,18 +6,8 @@ const JUMP_VELOCITY = 500.0
 
 var _gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var _direction_facing = Vector2.LEFT
-
-enum State {IDLE, MOVE, IN_AIR, WINDUP, SHOOT}
-
-var _state: State:
-	set(new_state):
-		if new_state != _state:
-			_state = new_state
-			set_animation(_state)
-
-
-var _winding_up_jump : bool = false
-var _on_ground : bool = false
+@onready var animation_tree : AnimationTree = $AnimationTree
+@onready var sprite : Sprite2D = $Sprite2D
 
 
 func _ready():
@@ -25,12 +15,9 @@ func _ready():
 
 
 func _process(delta):
-	if randi_range(1, 100) == 1 and is_on_floor():
-		wind_up_jump()
-
-	update_state()
-	if _state == State.IN_AIR:
-		set_animation(_state)
+	update_animation_parameters()
+	if randi_range(1, 100) == 1:
+		ready_jump()
 
 
 func _physics_process(delta):
@@ -45,56 +32,35 @@ func _physics_process(delta):
 			change_direction()
 
 
+func ready_jump() -> void:
+	animation_tree.set("parameters/conditions/is_jumping", true)
+
+
 func change_direction():
 	_direction_facing *= -1
-	$Sprite.flip_h = not $Sprite.flip_h
-
-
-func update_state() -> void:
-	if not is_on_floor():
-		_state = State.IN_AIR
-	elif _winding_up_jump:
-		_state = State.WINDUP
-	elif velocity.x != 0:
-		_state = State.MOVE
-	else:
-		_state = State.IDLE
-
-
-func set_animation(state : State) -> void:
-	match state:
-		State.IN_AIR:
-			choose_air_frame_based_on_speed()
-		State.WINDUP:
-			$Sprite.play("windup")
-		State.SHOOT:
-			$Sprite.play("shoot")
-		State.MOVE, _:
-			$Sprite.play("move")
-
-
-func choose_air_frame_based_on_speed():
-	$Sprite.animation = "jump_and_fall"
-	var absolute_velocity = abs(velocity.y)
-	
-	if absolute_velocity >= 250:
-		$Sprite.frame = 3
-	elif absolute_velocity >= 150:
-		$Sprite.frame = 2
-	else:
-		$Sprite.frame = 1
-
-
-func wind_up_jump() -> void:
-	_winding_up_jump = true
+	sprite.flip_h = not sprite.flip_h
 
 
 func jump() -> void:
 	if is_on_floor():
+		animation_tree.set("parameters/conditions/is_jumping", false)
 		velocity.y = -JUMP_VELOCITY
 
 
-func _on_sprite_animation_finished():
-	if $Sprite.animation == "windup" and _winding_up_jump:
-		_winding_up_jump = false
-		jump()
+func update_animation_parameters() -> void:
+	animation_tree.set("parameters/conditions/is_idle", 
+		is_on_floor() and velocity == Vector2.ZERO)
+	
+	animation_tree.set("parameters/conditions/is_in_air",
+		not is_on_floor())
+	
+	animation_tree.set("parameters/conditions/is_landing",
+		is_on_floor_only())
+	
+	animation_tree.set("parameters/conditions/is_moving",
+		velocity.x != 0)
+	
+	animation_tree.set("parameters/In Air/blend_position",
+		abs(velocity.y))
+
+
