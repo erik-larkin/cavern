b8a5a1874cf7d5c6ce719ff4c27fa4ef7d1f5607
erik-larkin <erik.larkin789@gmail.com>
2023-09-02 19:01:15 +0100

    make items collectable

--- scripts/item.gd
+++ scripts/item.gd
@@ -1,20 +1,46 @@
+@tool
+
 extends Node2D
 
-enum {APPLE, HEART, LEMON, LIFE, RASPBERRY}
+enum Types {APPLE, HEART, LEMON, LIFE, RASPBERRY}
+
+@export var type : Types
+
+var collected : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var animation : String = "";
-	match randi_range(APPLE, RASPBERRY):
-		APPLE:
-			animation = "apple"
-		HEART:
-			animation = "heart"
-		LEMON:
-			animation = "lemon"
-		LIFE:
-			animation = "life"
-		RASPBERRY:
-			animation = "raspberry"
+	set_animation()
+
+
+func _process(_delta):
+	if Engine.is_editor_hint():
+		set_animation()
+
+
+func set_animation() -> void:
+	var animation : String = ""
 	
+	match (type): 
+		Types.HEART:
+			animation = "heart"
+		Types.LEMON:
+			animation = "lemon"
+		Types.LIFE:
+			animation = "life"
+		Types.RASPBERRY:
+			animation = "raspberry"
+		Types.APPLE:
+			animation = "apple"
+			
 	$AnimationPlayer.play(animation)
+
+
+func _on_hitbox_body_entered(body):
+	if not collected:
+		collected = true
+		visible = false
+		$CollectSound.play()
+		$CollectSound.finished.connect(queue_free)
+
+
